39f034684f82bb5ebb56842a676e7ba93e53e4f4
Alvinfaisal <faisalvin15@gmail.com>
2021-08-05 15:48:57 +0700

    profil dan visi

--- profil.php
+++ profil.php
@@ -1,5 +1,58 @@
 <?php
-  include 'main.html';
+  include 'navbar.html';
+
+?>
+
+
+<div class="container mt-3">
+  <div class="row ">
+    <div class="col-4">
+        <ul>
+          <li><a href="#he1" class="text-dark">DUKUNGAN ASOSIASI PROFESI DAN ASOSIASI INDUSTRI</a></li>
+          <li><a href="#" class="text-dark">LEMBAGA SERTIFIKASI PROFESI SEBAGAI SERTIFIKATOR</a></li>
+          <li><a href="#" class="text-dark">DUKUNGAN ASOSIASI PROFESI DAN ASOSIASI INDUSTRI</a></li>
+          <li><a href="#" class="text-dark">PENGENDALIAN LSP</a></li>
+          <li><a href="#" class="text-dark">RUANG LINGKUP SKEMA YANG TERLISENSI BNSP</a></li>
+          <li><a href="#" class="text-dark">PENAMBAHAN RUANG LINGKUP</a></li>
+          <li><a href="#" class="text-dark">MITRA KERJA</a></li>
+          <li><a href="#" class="text-dark">LEMBAGA SERTIFIKASI PROFESI</a></li>
+          
+        </ul>
+    </div>
+    <div class="col-8">
+       <h3 id="he1">Dukungan Asosiasi Profesi dan Asosiasi Industri</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+       <p>Kegiatan yang di lakukan LSP TD antara lain adalah Sertifikasi SDM, pelatihan Asesor Kompetensi, Pembentukan TUK Mandiri, sosialisasi SKKNI dan penyelarasan kurikulum atau silabus pada dunia pendidikan dan pelatihan.</p>
+
+       <h3>LEMBAGA SERTIFIKASI PROFESI SEBAGAI SERTIFIKATOR</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>DUKUNGAN ASOSIASI PROFESI DAN ASOSIASI INDUSTRI</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>PENGENDALIAN LSP</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>RUANG LINGKUP SKEMA YANG TERLISENSI BNSP</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>PENAMBAHAN RUANG LINGKUP</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>MITRA KERJA</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+
+       <h3>LEMBAGA SERTIFIKASI PROFESI</h3>
+       <p>Mendapat dukungan dari Asosiasi profesi dan Asosiasi Industri, serta dukungan dari regulator Kementrian Komunikasi dan Informatika (Kominfo) menjadikan Kualitas dan kepercayaan masyarakat merupakan prioritas utama kami dalam melakukan proses sertifikasi. Dukungan dari Asesor baik dari kalangan Praktisi maupun Akademisi, serta kerjasama dengan Lembaga Pelatihan dan Perusahaan merupakan modal utama LSP TD untuk bersinergi dalam menghasilkan SDM yang memiliki kompetensi, diakui serta beretika sesuai dengan kebutuhan Dunia usaha dan Dunia Industri LSP TD juga terus melakukan kerjasama dengan pemangku kepentingan agar Sertifikasi betul-betul menjadi solusi bagi masalah SDM di Indonesia. </p>
+    </div>
+  </div>
+</div>
+
+
+
+
+<?php
+  include 'footer.html';
 
 ?>
 
---
+++ visi.php
@@ -0,0 +1,44 @@
+<?php
+  include 'navbar.html';
+
+?>
+
+
+<div class="container mt-3">
+  <div class="row ">
+    <div class="col-4">
+        <ul>
+          <li><a href="#intro" class="text-primary">Intro</a></li>
+          <li><a href="#visi" class="text-dark">Visi</a></li>
+          <li><a href="#misi" class="text-dark">Misi</a></li>
+          
+        </ul>
+    </div>
+    <div class="col-8">
+       <h3 id="intro">Visi & Misi</h3>
+       <img src="image/profil2.png" alt="gambar" style="width: 45rem">
+       <p class="mt-2">LSP Teknologi digital didirikan oleh Asosiasi, praktisi dan akademisi yang sudah berkecimpung lama di Sistem Sertifikasi Nasional. Pengalaman yang empiris dan harapan besar untuk mengantarkan Indonesia menjadi negara maju melalui SDM yang kompeten dan tersertifikasi, LSP TD akan terus komitmen, kontinyu dan konsisten menerapkan Sistem Manajemen Mutu pengelolaan Lembaga Sertifikasi. </p>
+
+       <h3 id="visi">Visi</h3>
+        <p>Menjadikan Lembaga Sertifikasi Profesi bidang teknologi informasi yang profesional, kredibel, akuntabel dan terkini dalam pengembangan dan perlindungan sumberdaya manusia</p>
+
+       <h3 id="misi">Misi</h3>
+        <ol>
+            <li>Menyenggarakan sertifikasi profesi/kompetensi bidang teknologi informasi yang di akui kredibilatsnya baik di dunia pendidikan maupun industri</li>
+            <li>Memiliki program kerja dan pengelolaan lembaga yang akuntabel dan dapat di pertanggungjawabkan dengan baik demi kepentingan pengembangan sumber daya manusia yang kompeten dan berakhlak mulia</li>
+            <li>Memastikan skema sertifikasi menggunakan standar kompetensi terkini dan dibutuhkan oleh dunia industri</li>
+            <li>Mengembangkan sumber daya manusia yang berkelanjutan dan berkesinambungan</li>
+            <li>Melindungi dan mengadvokasi tenaga kerja indonesia dari permasalahan hukum dan dari tenaga kerja asing yang masuk ke indonesia</li>
+        </ol>
+    </div>
+  </div>
+</div>
+
+
+
+
+<?php
+  include 'footer.html';
+
+?>
+
