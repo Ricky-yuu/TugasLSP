3525665820b408be2d2322933f04a8f856c2a45c
Ricky-yuu <meliodaskirisami@gmail.com>
2021-08-05 13:40:57 +0700

    tambah home

---
+++ home.php
@@ -0,0 +1,120 @@
+<?php
+  include 'main.html';
+
+?>
+
+<!-- carausel -->
+<div class="container">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!-- carausel -->
+
+<!-- conten 1 -->
+<div class="row bg-white p-4 text-black ">
+        <div class="col text-center"> 
+           Lembaga Sertifikasi Profesi 
+        </div>
+    </div>
+
+    <div class="row ">
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+
+      
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+    </div>
+<!-- end conten 1 -->
+
+
+
+<!-- conten 2 -->
+        <div class="row bg-white p-4 text-black ">
+        <div class="col text-center"> 
+           Daftar Skema sertifikasi 
+        </div>
+    </div>
+        <div class="row ">
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+
+      
+      <div class="col"> 
+        <div class="card" style="width: 18rem;">
+          <img src="image/foto3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> 
+      </div>
+    </div>
+
+  <!-- end conten 2 -->
